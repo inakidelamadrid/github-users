8c6d8b9aa140015982ce8c0ebef0d60b9018d7d6
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2023-02-02 19:07:36 -0600

    start using state to search the values in github

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import { Header } from "components/header";
+import { Heading } from "components/typography/headings";
+import { Search } from "components/search";
+import "./App.css";
 
 function App() {
-  return (<h1 className="text-3xl font-bold underline text-cyan-300">
-    Hello world!
-  </h1>)
+  const [searchValue, setSearchValue] = useState<string>("");
+  const handleSearchChange = (value: string) => {
+    setSearchValue(value);
+    console.log('Search value', value);
+  }
+	return (
+		<>
+			<Header />
+			<main className="max-w-screen-lg mx-auto">
+        <div className="flex">
+				  <Heading className="" level={1} text="Github Users" />
+          <Search className="ml-auto w-1/3" placeholder="Enter username or email" value={searchValue} onChange={handleSearchChange}/>
+        </div>
+			</main>
+		</>
+	);
 }
 
 export default App;
