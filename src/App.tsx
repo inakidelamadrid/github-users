d10e4ec364fe3e6841a4f4b1f9045aa4c0231781
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2023-02-02 19:28:44 -0600

    render my user from github

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import {useState} from "react";
+import { useQuery } from "react-query";
 import { Header } from "components/header";
 import { Heading } from "components/typography/headings";
 import { Search } from "components/search";
+import { UserListCard } from "components/user-list-card";
+import type {User} from '@saber2pr/types-github-api'
 import "./App.css";
 
 function App() {
@@ -10,6 +13,14 @@ function App() {
     setSearchValue(value);
     console.log('Search value', value);
   }
+  const { data } = useQuery('repoData', () =>
+     fetch('https://api.github.com/search/users?q=inakidelamadrid').then(res =>
+       res.json()
+     )
+   )
+  const users = (data?.items ?? []) as User[];
+  // use a placeholder service for missing avatars
+  const placeholderAvatar = 'https://via.placeholder.com/150';
 	return (
 		<>
 			<Header />
@@ -17,6 +28,12 @@ function App() {
         <div className="flex">
 				  <Heading className="" level={1} text="Github Users" />
           <Search className="ml-auto w-1/3" placeholder="Enter username or email" value={searchValue} onChange={handleSearchChange}/>
+
+        </div>
+        <div className='grid grid-cols-3'>
+          {users.map(user => {
+            return <UserListCard key={user.id} login={user.login} htmlUrl={user.html_url} avatar={{src: user.avatar_url ?? placeholderAvatar, alt: `${user.login} avatar`}}/>
+          })}
         </div>
 			</main>
 		</>
